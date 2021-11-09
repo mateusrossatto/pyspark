ac5cb6073460e4146153ce80c5a1c826ac59bc5d
Mateus Rossatto <84642985+mateusrossatto@users.noreply.github.com>
2021-11-08 23:55:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import streamlit as st
 
+df_primeira_visualizacao = pd.read_csv('data/primeira_visualizacao.csv')
+df_segunda_visualizacao = pd.read_csv('data/segunda_visualizacao.csv')
+df_terceira_visualizacao = pd.read_csv('data/terceira_visualizacao.csv')
+df_sintese_visualizacao = pd.read_csv('data/sintese_visualizacao.csv')
+
+st.write('Projeto Final de Spark - Campanha Nacional de Vacinação contra Covid-19')
+st.table(df_primeira_visualizacao)
+st.table(df_segunda_visualizacao)
+st.table(df_terceira_visualizacao)
+st.table(df_sintese_visualizacao)
